a3f087b23841506b7ec68e35ea7b470074f92ac2
David Li <david99999011@icloud.com>
2025-08-05 21:10:43 +1000

    finishing core parts

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
     let weightInput = document.querySelector("#weight");
     let heightInput = document.querySelector("#height");
     let bmiResultsValue = document.querySelector("#bmi-results-value");
+    let weightIdentifier = document.querySelector("#weight-identifier");
     unitSelect.addEventListener("change", () => {
         if (unitSelect.value === 'metric') {
             weightInput.placeholder = "Weight (kg)";
@@ -17,9 +18,10 @@ document.addEventListener("DOMContentLoaded", () => {
         let unit = unitSelect.value;
         let weight = parseFloat(weightInput.value);
         let height = parseFloat(heightInput.value);
+        let roundedBmi;
 
         if (isNaN(weight) || isNaN(height) || height === 0) {
-            document.querySelector("#bmi-results-value").innerHTML = "Invalid Input";
+            bmiResultsValue.textContent = "Invalid Input";
             bmiResultsValue.style.fontSize = "21px";
             return;
         }
@@ -33,11 +35,51 @@ document.addEventListener("DOMContentLoaded", () => {
             height = height * 0.0254; // Convert inches to meters
             bmi = weight / (height * height);
         }
-        bmiResultsValue.textContent = Math.round(bmi); // Round to one decimal place
-        bmiResultsValue.style.fontSize = 110 / String(Math.round(bmi)).length + "px";
+        roundedBmi = Math.round(bmi * 10) / 10; // Round to one decimal place
+        bmiResultsValue.textContent = roundedBmi;
+        bmiResultsValue.style.fontSize = 110 / String(roundedBmi).length + "px";
+        if (bmi < 18.5) {
+            weightIdentifier.style.color = "#c38902ff";
+            weightIdentifier.textContent = "underweight.";
+        } else if (bmi >= 18.5 && bmi < 24.9) {
+            weightIdentifier.style.color = "#00a600";
+            weightIdentifier.textContent = "healthy.";
+        } else if (bmi >= 25 && bmi < 29.9) {
+            weightIdentifier.style.color = "#a70101ff";
+            weightIdentifier.textContent = "overweight.";
+        } else if (bmi >= 30) {
+            weightIdentifier.style.color = "#ff0000ff";
+            weightIdentifier.textContent = "obese.";
+        }
+    }
+
+    function changeUnit() {
+        update();
+        if (unitSelect.value === 'metric') {
+            weightInput.value = '';
+            heightInput.value = '';
+            bmiResultsValue.textContent = "Waiting for Input";
+            weightIdentifier.textContent = "...";
+        } else if (unitSelect.value === 'imperial') {
+            weightInput.value = '';
+            heightInput.value = '';
+            bmiResultsValue.textContent = "Waiting for Input";
+            weightIdentifier.textContent = "...";
+        }
     }
 
     weightInput.addEventListener("input", update);
     heightInput.addEventListener("input", update);
-    unitSelect.addEventListener("change", update);
+    unitSelect.addEventListener("change", changeUnit);
+
+    let disclaimer = document.querySelector("#disclaimer");
+    setTimeout(() => {
+        disclaimer.style.transform = "translateY(0px)";
+    }, 200);
+    setTimeout(() => {
+        disclaimer.style.transform = "translateY(-95px)";
+    }, 5000);
+    disclaimer.addEventListener("click", () => {
+         disclaimer.style.transform = "translateY(-95px)";
+    })
 });
